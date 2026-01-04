e6b4ba08ef494fe6161375b3b9b5a5d245a92180
kazanefu <kazanefu12@gmail.com>
2026-01-04 17:16:59 +0900

    fix day 13

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bff69f66.js',
+                path_to_root + 'searchindex-17e29bd5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
